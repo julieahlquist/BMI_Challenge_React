04defd1716b431f6825de539fdae13ba8098b871
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-05 10:54:51 +0200

    adds display result class and fetches state from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DisplayResult from './src/Components/displayResult';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class App extends Component {
       height: ''
     }
   }
-  
+
   render() {
     return (
       <div>
@@ -21,6 +22,11 @@ class App extends Component {
           <label>Height(cm)</label>
           <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
         </div>
+
+        <DisplayResult
+        weight={this.state.weight}
+        height={this.state.height}
+        />
       </div>
     );
   }
