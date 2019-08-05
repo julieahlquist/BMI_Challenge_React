de587f1aa49758086c0e18511f43b8b0b1c766d9
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-05 16:55:38 +0200

    removes breaking code snippet

--- src/App.js
+++ src/App.js
@@ -34,16 +34,6 @@ class App extends Component {
           <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
         </div>
 
-        <div>
-          <label>Weight(kg)</label>
-          <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value})} />
-        </div>
-
-        <div>
-          <label>Height(cm)</label>
-          <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
-        </div>
-
         <DisplayResult
         weight={this.state.weight}
         height={this.state.height}
